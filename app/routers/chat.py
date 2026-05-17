e67878150ced212ecba94e6409fa133f87460cbb
mawilla2 <mawilla15810008640@outlook.com>
2026-05-17 17:28:50 +0900

    成功对接OPENAI接口

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from app.schemas.chat_schema import ChatRequest, ChatResponse
-from app.services.chat_service import generate_reply
+from app.services.chat_service import  generate_ai_reply
 
 router = APIRouter()
 
 @router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
     
-    reply = generate_reply(request.message)
+    reply = generate_ai_reply(request.message)
 
     return ChatResponse(reply=reply)
--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -1,6 +1,41 @@
-def generate_reply(message: str) -> str:
-    
-    if "你好" in message:
-        return "您好，请问有什么可以帮您？"
-    
-    return "抱歉，我暂时无法理解您的问题"
+import os
+from openai import OpenAI
+from openai import RateLimitError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+def generate_ai_reply(message: str) -> str:
+
+    try:
+
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "你是一个专业AI客服"
+                },
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ]
+        )
+
+        content = response.choices[0].message.content
+
+        if content is None:
+            return "AI没有返回内容"
+
+        return content
+
+    except RateLimitError:
+        return "当前AI服务额度不足，请稍后再试"
+
+    except Exception as e:
+        return f"发生错误: {str(e)}"
